32e9d86b2c3328b77b0d060f544e63eb6b527254
Parminder Kaur <parminder_jk@hotmail.com>
2018-05-01 22:49:50 -0400

    add ui component for upload

--- wave-payroll-backend/app.js
+++ wave-payroll-backend/app.js
@@ -3,6 +3,7 @@
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
 var multer  = require('multer');
+var cors = require('cors');
 
 module.exports = app; // for testing
 
@@ -12,6 +13,32 @@ app.use(upload.fields([{ name: 'file' }]));
 var config = {
   appRoot: __dirname // required config
 };
+
+// Enable CORS middleware
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
+
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, Authorization, Content-Type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
--- wave-payroll-frontend/src/components/FileUpload.js
+++ wave-payroll-frontend/src/components/FileUpload.js
@@ -1,13 +1,67 @@
 import React, { Component } from 'react';
-import { ButtonToolbar, Button } from 'react-bootstrap';
+import { ButtonToolbar, Button, PageHeader, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
+import './../App.css';
+import axios from 'axios';
 
 class FileUpload extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.uploadHandler = this.uploadHandler.bind(this);
+  }
+
+  uploadHandler(){
+    console.log(this.state.selectedFile);
+    let file = this.state.selectedFile;
+    if(file){
+      const data = new FormData();
+      data.append('file', file, { type: 'text/csv' });
+      axios.post('http://localhost:10010/upload', data)
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  }
+
+
+  handleChange(e){
+    let name = e.target.name;
+    let value = e.target.value;
+    console.log(name);
+    console.log(value);
+    console.log(e.target.files);
+    this.setState({ selectedFile: e.target.files[0] || null });
+    //this.uploadHandler(e.target.files[0]);
+  }
+
   render(){
     return(
-      <div>
-        <ButtonToolbar>
-          <Button bsStyle="success">Success</Button>
+      <div className="page-container">
+        <PageHeader>
+          Wave Payroll Web Application
+        </PageHeader>
+
+        <FormGroup>
+          <ControlLabel>Select file</ControlLabel>
+          <FormControl
+            type="file"
+            accept=".csv"
+            onChange={this.handleChange.bind(this)}
+          />
+        </FormGroup>
+
+        <ButtonToolbar className="section">
+          <Button bsStyle="success" onClick={this.uploadHandler()}>Upload</Button>
         </ButtonToolbar>
+
       </div>
     );
   }
