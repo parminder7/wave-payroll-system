627fa15fcaef730271f998de8010e25e9ffaa0a1
Parminder Kaur <parminder_jk@hotmail.com>
2018-05-04 22:35:15 -0400

    added logic - abort load to database if report id already exists

--- wave-payroll-backend/api/lib/save_to_db_deamon.js
+++ wave-payroll-backend/api/lib/save_to_db_deamon.js
@@ -16,6 +16,8 @@ logger.level = 'debug';
 
 const REPORT_ID_FIELD = 'report id';
 
+var ABORT_LOAD = false;
+
 // exported methods to access outside file
 module.exports = {
   process_pending_jobs: process_pending_jobs
@@ -43,18 +45,28 @@ function process_files(conn, callback){
     logger.info(`ADD event triggered by file ${fpath}`);
     async.waterfall([
       async.apply(parse_csv, fpath),
+      async.apply(check_if_report_exists, conn),
       async.apply(load_to_db, conn)
     ], function(err){
       if (err){
         logger.error(`Error parsing and loading data from CSV to database ${err}`);
-        let to = path.join(config.errorDir, fpath.replace(/^.*[\\\/]/, ''));
-        movefile(fpath, to, function(error){
-          if (err) return callback(error);
-          return callback(err);
-        });
+        // don't move file to error dir when aborting on duplicate report id
+        if(ABORT_LOAD){
+          removefile(fpath, callback);
+        }
+        else{
+          // else push file to error dir for retry (if needed in future)
+          let to = path.join(config.errorDir, fpath.replace(/^.*[\\\/]/, ''));
+          movefile(fpath, to, function(error){
+            if (err) return callback(error);
+            return callback(err);
+          });
+        }
       }
-      // remove file after processing is done
-      removefile(fpath, callback);
+      else{
+        // remove file after processing is done
+        removefile(fpath, callback);
+      }
     });
     })
     .on('error', error => {
@@ -96,7 +108,7 @@ function parse_csv(path, callback){
       let isfooter = false;
       headers.forEach( header => {
         // special handling for report id footer
-        if (row[header] === REPORT_ID_FIELD){ //TODO: fail if report id already exists
+        if (row[header] === REPORT_ID_FIELD){
           isfooter = true;
         }
         if (isfooter && row[header]) {
@@ -119,7 +131,7 @@ function parse_csv(path, callback){
     })
     .on('end',function() {
       logger.info(`Parsing completed - ${path}`);
-      return callback(null, data, report_id);
+      return callback(null, {data: data, report_id: report_id});
     })
     .on('error', function(err){
       logger.error(`Error occured while parsing CSV file ${path}`);
@@ -138,11 +150,8 @@ function get_pay_date(arow){
   // date format is 'yyyy-mm-dd'
   let mysql_date = given_date.split('-');
 
-  console.log(mysql_date);
-
   // parseInt("", radix) radix is 10 for decimal
   let day = parseInt(mysql_date[2], 10);
-  console.log("day= " + day);
 
   let pay_date;
   if (day >= 1 && day <= 15){
@@ -152,7 +161,7 @@ function get_pay_date(arow){
     pay_date = mysql_date[0] + '-' + mysql_date[1] + '-' +
       utils.get_days_by_month(mysql_date[1], mysql_date[0]);
   }
-  console.log('pay-date = ' + pay_date);
+
   return pay_date;
 }
 
@@ -161,17 +170,17 @@ function append_pay_period_col(data){
     // get_pay_date(arow);
     return arow.push(get_pay_date(arow));
   });
-  console.log(resultant);
+
   return resultant;
 }
 
-function load_to_db(conn, data, report_id, callback){
+function load_to_db(conn, obj, callback){
+  let data = obj.data;
+
   // push report id to each row for sql query
   _.map(data, function(row){
-    return row.push(report_id);
+    return row.push(obj.report_id);
   });
-
-  console.log(data);
 
   // add extra column for pay day for each row
   // data = append_pay_period_col(data);
@@ -192,6 +201,45 @@ function load_to_db(conn, data, report_id, callback){
     // conn.end();
     logger.info(`load_to_db insertion succeed`);
     return callback(null);
+  });
+}
+
+function check_if_report_exists(conn, obj,callback){
+  async.waterfall([
+    async.apply(get_report_ids, conn),
+    function(list, callback){
+      let found = false;
+      _.forEach(list, report => {
+        if(report.report_id == obj.report_id){
+          found = true;
+          return false;
+        }
+      });
+
+      if(found){
+        ABORT_LOAD = true;
+        return callback(new Error(`Report id already exists. Aborted upload.`));
+      }
+      else
+        return callback(null);
+    }
+  ], function(err){
+    if(err)
+      return callback(err);
+    return callback(null, obj);
+  });
+}
+
+function get_report_ids(conn, callback) {
+  let query = 'SELECT DISTINCT(report_id) FROM timesheet';
+  conn.query(query, function(err, result) {
+    if (err) {
+      logger.error(`get_report_ids selection failed, query: ${query}`);
+      return callback(err);
+    }
+    // conn.end();
+    logger.info(`get_report_ids selection succeed`);
+    return callback(null, result);
   });
 }
 
