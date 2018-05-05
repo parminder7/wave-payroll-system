3e7c9af9a00dc1f8f0c9c84a3f42615ed97dd926
Parminder Kaur <parminder_jk@hotmail.com>
2018-05-04 22:34:32 -0400

    fix - close db connection on error

--- wave-payroll-backend/api/helpers/database_utils.js
+++ wave-payroll-backend/api/helpers/database_utils.js
@@ -9,20 +9,38 @@ module.exports = {
   connect_to_database: connect_to_database
 };
 
+var db_config = {
+  host     : config.dbHost,
+  user     : config.dbUser,
+  password : config.dbPassword,
+  database : config.dbName
+};
+
+
 function connect_to_database(callback){
-  let connection = mysql.createConnection({
-    host     : config.dbHost,
-    user     : config.dbUser,
-    password : config.dbPassword,
-    database : config.dbName
-  });
+  let connection = mysql.createConnection(db_config);
+
+  handle_disconnect(connection);
 
   connection.connect(function(err){
     if (err){
       logger.error(`Database connection failed! ${err}`);
+      connection.end(function(err) {
+        // The connection is terminated now
+      });
       return callback(err);
     }
     logger.info('Connected to database : %s', config.dbHost);
     return callback(null, connection);
+  });
+}
+
+function handle_disconnect(connection) {
+  connection.on('error', function(error) {
+    logger.error(`Handle disconnect on error - ${error}`);
+
+    connection.end(function(err) {
+      // The connection is terminated now
+    });
   });
 }
